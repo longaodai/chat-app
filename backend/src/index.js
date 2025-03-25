2e7b80fac1ae501594b8242c4352a3ba106d3783
Võ Chí Long (Long Áo Dài) <87090422+longaodai@users.noreply.github.com>
2025-03-25 13:30:27 +0700

    :tada: hotfix arrange routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -26,9 +30,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
 
 server.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
